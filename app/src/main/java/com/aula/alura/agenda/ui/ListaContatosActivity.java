1e84cdd8bdbe7db592d11b8a223c14ba2796fbf5
Angelica dos Santos <santosangelicassp@gmail.com>
2021-08-13 10:05:07 -0300

    nova tela para cadastro de contatos

--- app/src/main/java/com/aula/alura/agenda/ui/ListaContatosActivity.java
+++ app/src/main/java/com/aula/alura/agenda/ui/ListaContatosActivity.java
@@ -1,28 +1,34 @@
-package com.aula.alura.agenda;
+package com.aula.alura.agenda.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.aula.alura.agenda.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class ListaContatosActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_contatos);
+
+        ListView lista = findViewById(R.id.lv_lista);
+        FloatingActionButton fabNovoContato = findViewById(R.id.fab_novo_contato);
+
+        fabNovoContato.setColorFilter(Color.WHITE);
 
         List<String> alunos = new ArrayList<>(
                 Arrays.asList("Angelica", "Luiza", "Helena")
         );
-
-        ListView lista = findViewById(R.id.lv_lista);
 
         lista.setAdapter(new ArrayAdapter<>(
                 this,
---
+++ app/src/main/java/com/aula/alura/agenda/ui/NovoContatoActivity.java
@@ -0,0 +1,16 @@
+package com.aula.alura.agenda.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aula.alura.agenda.R;
+
+public class NovoContatoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_novo_contato);
+    }
+}
