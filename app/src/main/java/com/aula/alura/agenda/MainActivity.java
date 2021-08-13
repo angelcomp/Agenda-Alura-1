879802b25747ec318c1d04929608389623fd41a8
Angelica dos Santos <santosangelicassp@gmail.com>
2021-08-13 09:42:34 -0300

    melhorias usando adapter e listview

--- app/src/main/java/com/aula/alura/agenda/MainActivity.java
+++ app/src/main/java/com/aula/alura/agenda/MainActivity.java
@@ -3,6 +3,8 @@ package com.aula.alura.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -20,12 +22,12 @@ public class MainActivity extends AppCompatActivity {
                 Arrays.asList("Angelica", "Luiza", "Helena")
         );
 
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
+        ListView lista = findViewById(R.id.lv_lista);
 
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+        lista.setAdapter(new ArrayAdapter<>(
+                this,
+                android.R.layout.simple_list_item_1,
+                alunos
+        ));
     }
 }
