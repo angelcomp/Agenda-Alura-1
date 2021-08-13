d3b926c2e93efc9da2d1891a5481861cbd38b21e
Angelica dos Santos <santosangelicassp@gmail.com>
2021-08-13 14:01:46 -0300

    criando novo contato

---
+++ app/src/main/java/com/aula/alura/agenda/models/Pessoa.java
@@ -0,0 +1,26 @@
+package com.aula.alura.agenda.models;
+
+public class Pessoa {
+
+    private final String nome;
+    private final String telefone;
+    private final String email;
+
+    public Pessoa(String nome, String telefone, String email) {
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
--- app/src/main/java/com/aula/alura/agenda/ui/NovoContatoActivity.java
+++ app/src/main/java/com/aula/alura/agenda/ui/NovoContatoActivity.java
@@ -1,10 +1,15 @@
 package com.aula.alura.agenda.ui;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 import com.aula.alura.agenda.R;
+import com.aula.alura.agenda.models.Pessoa;
 
 public class NovoContatoActivity extends AppCompatActivity {
 
@@ -12,5 +17,28 @@ public class NovoContatoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_contato);
+
+        EditText campoNome = findViewById(R.id.et_nome);
+        EditText campoTelefone = findViewById(R.id.et_telefone);
+        EditText campoEmail = findViewById(R.id.et_email);
+        Button btnSalvar = findViewById(R.id.btn_salvar);
+
+
+        btnSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String nome = campoNome.getText().toString();
+                String telefone = campoTelefone.getText().toString();
+                String email = campoEmail.getText().toString();
+
+                Pessoa contato = new Pessoa(nome, telefone, email);
+
+                Toast.makeText(NovoContatoActivity.this,
+                        contato.getNome() + " - " +
+                                contato.getTelefone() + " - " +
+                                contato.getEmail(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
