ac11b8c7598b3cd358e1700ae8fc0975d9407f0d
Angelica dos Santos <santosangelicassp@gmail.com>
2021-08-13 09:38:36 -0300

    lista de contatos simples

--- app/src/main/java/com/aula/alura/agenda/MainActivity.java
+++ app/src/main/java/com/aula/alura/agenda/MainActivity.java
@@ -3,6 +3,11 @@ package com.aula.alura.agenda;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Angelica", "Luiza", "Helena")
+        );
+
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
